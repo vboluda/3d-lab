00eabb2a890d592229a6e292d29d9e11d3f9cea8
Vicente Boluda Vias <vicente.boludavias@gmail.com>
2018-12-21 22:37:29 +0100

    earth3d example

--- earth3d/lib/earth3d.js
+++ earth3d/lib/earth3d.js
@@ -10,7 +10,7 @@ GeoPosition.prototype.LABEL = "_AUX_SPPOS";
 // Constructor
 function GeoPosition(opts) {
     opts = opts || {};
-    this.texture = opts.texture || './resources/world_black.jpg';
+    this.texture = opts.texture || 'resources/world_black.jpg';
     this.color = opts.color || function (mag) {
         return 0xFF9900;// + Math.random() * 128
     };
